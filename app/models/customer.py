a7d596379f904a0f14a12a8ad965f1f766d48614
Craig Russell <craig@craig-russell.co.uk>
2015-07-17 20:18:45 +0100

    Added fields to customer with tests

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,16 +3,20 @@
 # Author: Craig Russell <craig@craig-russell.co.uk>
 # Customer Model
 
+import uuid
 from app import db
+from datetime import datetime as DT
 
 class Customer(db.Model):
-    id   = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    
-    # TODO sites
-    # TODO enabled
-    # TODO created at
-    # TODO modified at
+    __tablename__ = "customers"
+
+    id         = db.Column(db.Integer, primary_key=True)
+    name       = db.Column(db.String(80))
+    uuid       = db.Column(db.String(80), default=lambda: str(uuid.uuid4()))
+    sites      = db.Column(db.PickleType(), default=[])
+    enabled    = db.Column(db.Boolean(), default=True)
+    created_at = db.Column(db.DateTime, default=DT.now())
+    updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
     # TODO Customer <has> Users
     # TODO Customer <has> Placeholders
@@ -21,4 +25,5 @@ class Customer(db.Model):
     # TODO Customer <has> Demographics
 
     def __repr__(self):
-        return 'Customer[%r] %r' % (self.id, self.name)
+        return 'Customer[%r] %r, %r, %r, %r, %r, %r' % \
+            (self.id, self.name, self.uuid, self.sites, self.enabled, self.created_at, self.updated_at)
--- test/test_models/test_customer.py
+++ test/test_models/test_customer.py
@@ -4,6 +4,7 @@
 # Unit Tests for Customer Model
 
 import unittest
+import datetime as DT
 from app import db
 from app.models import Customer
 
@@ -11,20 +12,35 @@ class TestModelCustomer(unittest.TestCase):
 
     def test_crud(self):
         # Create
-        customer = Customer(name='Foo')
+        customer = Customer(name='Foo', sites=['foo', 'bar'])
         db.session.add(customer)
         db.session.commit()
         self.assertIn(customer, Customer.query.all())
+        self.assertIsInstance(customer.uuid, unicode)
+        self.assertIsInstance(customer.sites, list)
+        self.assertIsInstance(customer.enabled, bool)
+        self.assertIsInstance(customer.created_at, DT.datetime)
+        self.assertIsInstance(customer.updated_at, DT.datetime)
 
         # Read
         customer = Customer.query.filter_by(name='Foo').first()
         self.assertEqual(customer.name, 'Foo')
+        self.assertEqual(customer.sites, ['foo', 'bar'])
+        self.assertTrue(customer.enabled)
         
         # Update
+        old_created_at = customer.created_at
+        old_updated_at = customer.updated_at
         customer.name = 'Bar'
+        customer.sites = ['spam', 'eggs']
+        customer.enabled = False
         customer = Customer.query.filter_by(name='Bar').first()
         self.assertIsInstance(customer, Customer)
-        self.assertEqual('Bar', customer.name)
+        self.assertEqual(customer.name, 'Bar')
+        self.assertEqual(customer.sites, ['spam', 'eggs'])
+        self.assertFalse(customer.enabled)
+        self.assertEqual(customer.created_at, old_created_at)
+        self.assertNotEqual(customer.updated_at, old_updated_at)
 
         # Delete
         db.session.delete(customer)
