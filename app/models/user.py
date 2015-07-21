3f51a4aa09039eafbf7126b36a31ec575a5e2bf6
Craig Russell <craig@craig-russell.co.uk>
2015-07-21 15:25:19 +0100

    Added fields to user model with tests

--- app/models/user.py
+++ app/models/user.py
@@ -3,21 +3,22 @@
 # Author: Craig Russell <craig@craig-russell.co.uk>
 # High-level models
 
+from datetime import datetime as DT
 from app import db
 
 class User(db.Model):
     __tablename__ = "users"
     
-    id   = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-
-    # TODO email
-    # TODO password
-    # TODO is admin
-    # TODO enabled
-    # TODO created at
-    # TODO modified at
-    # TODO last logged in
+    id         = db.Column(db.Integer, primary_key=True)
+    name       = db.Column(db.String(80))
+    email      = db.Column(db.String(80))
+    password   = db.Column(db.String(218))
+    enabled    = db.Column(db.Boolean(), default=True)
+    admin      = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime, default=DT.now())
+    updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
+    last_login = db.Column(db.DateTime)
 
     def __repr__(self):
-        return 'User[%r] %r' % (self.id, self.name)
+        return 'User[%r] %r, %r, %r, %r, %r, %r, %r' % \
+            (self.id, self.name, self.email, self.enabled, self.is_admin, self.created_at, self.updated_at, self.last_login)
--- test/test_models/test_user.py
+++ test/test_models/test_user.py
@@ -4,6 +4,7 @@
 # Unit Tests for User Model
 
 import unittest
+import datetime as DT
 from app import db
 from app.models import User
 
@@ -11,20 +12,46 @@ class TestModelUser(unittest.TestCase):
 
     def test_user_crud(self):
         # Create
-        user = User(name='Foo')
+        user = User(name='Foo', email='a@b.com', password='passw0rd')
         db.session.add(user)
         db.session.commit()
         self.assertIn(user, User.query.all())
+        self.assertIsInstance(user.name, unicode)
+        self.assertIsInstance(user.email, unicode)
+        self.assertIsInstance(user.password, unicode)
+        self.assertIsInstance(user.enabled, bool)
+        self.assertIsInstance(user.admin, bool)
+        self.assertIsInstance(user.created_at, DT.datetime)
+        self.assertIsInstance(user.updated_at, DT.datetime)
+        self.assertIsInstance(user.last_login, type(None)) # No timestamp before first login
 
         # Read
         user = User.query.filter_by(name='Foo').first()
         self.assertEqual(user.name, 'Foo')
+        self.assertEqual(user.email, 'a@b.com')
+        self.assertEqual(user.password, 'passw0rd')
+        self.assertTrue(user.enabled)
+        self.assertFalse(user.admin)
         
         # Update
-        user.name = 'Bar'
+        old_created_at  = user.created_at
+        old_updated_at  = user.updated_at
+        user.name       = 'Bar'
+        user.email      = 'c@d.com'
+        user.password   = 'qwerty'
+        user.enabled    = False
+        user.admin      = True
+        user.last_login = DT.datetime.now()
         user = User.query.filter_by(name='Bar').first()
         self.assertIsInstance(user, User)
         self.assertEqual('Bar', user.name)
+        self.assertEqual(user.email, 'c@d.com')
+        self.assertEqual(user.password, 'qwerty')
+        self.assertFalse(user.enabled)
+        self.assertTrue(user.admin)
+        self.assertEqual(user.created_at, old_created_at)
+        self.assertNotEqual(user.updated_at, old_updated_at)
+        self.assertIsInstance(user.last_login, DT.datetime)
 
         # Delete
         db.session.delete(user)
