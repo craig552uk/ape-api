d2278c25ef80fdcb60c73036f3f4cd63c939c45a
Craig Russell <craig@craig-russell.co.uk>
2015-07-22 15:44:28 +0100

    Added fields to component model

--- app/models/component.py
+++ app/models/component.py
@@ -3,6 +3,7 @@
 # Author: Craig Russell <craig@craig-russell.co.uk>
 # High-level models
 
+from datetime import datetime as DT
 from app import db
 
 class Component(db.Model):
@@ -10,12 +11,12 @@ class Component(db.Model):
 
     id             = db.Column(db.Integer, primary_key=True)
     placeholder_id = db.Column(db.Integer, db.ForeignKey('placeholders.id'))
+    index          = db.Column(db.Integer)
     name           = db.Column(db.String(80))
-
-    # TODO order
-    # TODO markup
-    # TODO created_at
-    # TODO updated_at
+    markup         = db.Column(db.String(80))
+    created_at     = db.Column(db.DateTime, default=DT.now())
+    updated_at     = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
     def __repr__(self):
-        return 'Component[%r] %r' % (self.id, self.name)
+        return 'Component[%r] %r, %r, %r, %r' % \
+            (self.id, self.name, self.index, self.created_at, self,updated_at)
--- test/test_models/test_component.py
+++ test/test_models/test_component.py
@@ -4,6 +4,7 @@
 # Unit Tests for Component Model
 
 import unittest
+import datetime as DT
 from app import db
 from app.models import Component
 
@@ -11,20 +12,34 @@ class TestModelComponent(unittest.TestCase):
 
     def test_component_crud(self):
         # Create
-        component = Component(name='Foo')
+        component = Component(name='Foo', index=5, markup="<h1>Hello</h1>")
         db.session.add(component)
         db.session.commit()
         self.assertIn(component, Component.query.all())
+        self.assertIsInstance(component.index, int)
+        self.assertIsInstance(component.markup, unicode)
+        self.assertIsInstance(component.created_at, DT.datetime)
+        self.assertIsInstance(component.updated_at, DT.datetime)
 
         # Read
         component = Component.query.filter_by(name='Foo').first()
         self.assertEqual(component.name, 'Foo')
+        self.assertEqual(component.index, 5)
+        self.assertEqual(component.markup, "<h1>Hello</h1>")
         
         # Update
-        component.name = 'Bar'
+        old_created_at = component.created_at
+        old_updated_at = component.updated_at
+        component.name   = 'Bar'
+        component.index  = 7
+        component.markup = "<h2>Bye</h2>"
         component = Component.query.filter_by(name='Bar').first()
         self.assertIsInstance(component, Component)
-        self.assertEqual('Bar', component.name)
+        self.assertEqual(component.name, "Bar")
+        self.assertEqual(component.index, 7)
+        self.assertEqual(component.markup, "<h2>Bye</h2>")
+        self.assertEqual(component.created_at, old_created_at)
+        self.assertNotEqual(component.updated_at, old_updated_at)
 
         # Delete
         db.session.delete(component)
