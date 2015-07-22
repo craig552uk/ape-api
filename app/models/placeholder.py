c2aeba32e34ebeb3b5b40435c926c05d20bed9e7
Craig Russell <craig@craig-russell.co.uk>
2015-07-22 15:32:07 +0100

    Added fields to placeholder model

--- app/models/placeholder.py
+++ app/models/placeholder.py
@@ -3,6 +3,8 @@
 # Author: Craig Russell <craig@craig-russell.co.uk>
 # Placeholder Model
 
+import uuid
+from datetime import datetime as DT
 from sqlalchemy.orm import relationship
 from app import db
 
@@ -12,12 +14,12 @@ class Placeholder(db.Model):
     id         = db.Column(db.Integer, primary_key=True)
     account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
     name       = db.Column(db.String(80))
+    uuid       = db.Column(db.String(80), default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=DT.now())
+    updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
     components = relationship("Component", backref="placeholder")
 
-    # TODO uuid (element class)
-    # TODO created_at
-    # TODO modified_at
-
     def __repr__(self):
-        return 'Placeholder[%r] %r' % (self.id, self.name)
+        return 'Placeholder[%r] %r, %r, %r, %r' % \
+            (self.id, self.name, self.uuid, self.created_at, self.updated_at)
--- test/test_models/test_placeholder.py
+++ test/test_models/test_placeholder.py
@@ -4,6 +4,7 @@
 # Unit Tests for Placeholder Model
 
 import unittest
+import datetime as DT
 from app import db
 from app.models import Placeholder
 
@@ -15,16 +16,23 @@ class TestModelPlaceholder(unittest.TestCase):
         db.session.add(placeholder)
         db.session.commit()
         self.assertIn(placeholder, Placeholder.query.all())
+        self.assertIsInstance(placeholder.uuid, unicode)
+        self.assertIsInstance(placeholder.created_at, DT.datetime)
+        self.assertIsInstance(placeholder.updated_at, DT.datetime)
 
         # Read
         placeholder = Placeholder.query.filter_by(name='Foo').first()
         self.assertEqual(placeholder.name, 'Foo')
         
         # Update
+        old_created_at = placeholder.created_at
+        old_updated_at = placeholder.updated_at
         placeholder.name = 'Bar'
         placeholder = Placeholder.query.filter_by(name='Bar').first()
         self.assertIsInstance(placeholder, Placeholder)
         self.assertEqual('Bar', placeholder.name)
+        self.assertEqual(placeholder.created_at, old_created_at)
+        self.assertNotEqual(placeholder.updated_at, old_updated_at)
 
         # Delete
         db.session.delete(placeholder)
