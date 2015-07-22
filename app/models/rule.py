ab25dd41978e3e08a393fff71dc9d3af2df0841c
Craig Russell <craig@craig-russell.co.uk>
2015-07-22 16:22:35 +0100

    Added fields to rule  model

--- app/models/rule.py
+++ app/models/rule.py
@@ -3,6 +3,7 @@
 # Author: Craig Russell <craig@craig-russell.co.uk>
 # Rule models
 
+from datetime import datetime as DT
 from app import db
 
 class Rule(db.Model):
@@ -10,14 +11,13 @@ class Rule(db.Model):
     
     id         = db.Column(db.Integer, primary_key=True)
     segment_id = db.Column(db.Integer, db.ForeignKey('segments.id'))
-    name       = db.Column(db.String(80))
-    
-    # TODO group_id
-    # TODO field
-    # TODO comparator
-    # TODO value
-    # TODO created_at
-    # TODO modified_at
+    group_id   = db.Column(db.Integer)
+    field      = db.Column(db.String(80))
+    comparator = db.Column(db.String(80))
+    value      = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=DT.now())
+    updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
     def __repr__(self):
-        return 'Rule[%r] %r' % (self.id, self.name)
+        return 'Rule[%r] %r, %r, %r, %r, %r, %r, %r' % \
+            (self.id, self.name, self.segment_id, self.group_id, self.field, self.comparator, self.value, self.created_at, self.updated_at)
--- test/test_models/test_rule.py
+++ test/test_models/test_rule.py
@@ -4,6 +4,7 @@
 # Unit Tests for Rule Model
 
 import unittest
+import datetime as DT
 from app import db
 from app.models import Rule
 
@@ -11,22 +12,41 @@ class TestModelRule(unittest.TestCase):
 
     def test_rule_crud(self):
         # Create
-        rule = Rule(name='Foo')
+        rule = Rule(group_id=3, field="foo", comparator="EQUAL", value="foo bar")
         db.session.add(rule)
         db.session.commit()
         self.assertIn(rule, Rule.query.all())
+        self.assertIsInstance(rule.group_id, int)
+        self.assertIsInstance(rule.field, unicode)
+        self.assertIsInstance(rule.comparator, unicode)
+        self.assertIsInstance(rule.value, unicode)
+        self.assertIsInstance(rule.created_at, DT.datetime)
+        self.assertIsInstance(rule.updated_at, DT.datetime)
 
         # Read
-        rule = Rule.query.filter_by(name='Foo').first()
-        self.assertEqual(rule.name, 'Foo')
+        rule = Rule.query.filter_by(id=rule.id).first()
+        self.assertEqual(rule.group_id, 3)
+        self.assertEqual(rule.field, "foo")
+        self.assertEqual(rule.comparator, "EQUAL")
+        self.assertEqual(rule.value, "foo bar")
         
         # Update
-        rule.name = 'Bar'
-        rule = Rule.query.filter_by(name='Bar').first()
+        old_created_at  = rule.created_at
+        old_updated_at  = rule.updated_at
+        rule.group_id   = 5
+        rule.field      = "bar"
+        rule.comparator = "NOT_EQUAL"
+        rule.value      = "bar foo"
+        rule = Rule.query.filter_by(id=rule.id).first()
         self.assertIsInstance(rule, Rule)
-        self.assertEqual('Bar', rule.name)
+        self.assertEqual(rule.group_id, 5)
+        self.assertEqual(rule.field, "bar")
+        self.assertEqual(rule.comparator, "NOT_EQUAL")
+        self.assertEqual(rule.value, "bar foo")
+        self.assertEqual(rule.created_at, old_created_at)
+        self.assertNotEqual(rule.updated_at, old_updated_at)
 
         # Delete
         db.session.delete(rule)
-        count = Rule.query.filter_by(name='Bar').count()
+        count = Rule.query.filter_by(id=rule.id).count()
         self.assertEqual(0, count)
