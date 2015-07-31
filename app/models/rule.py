7e8cc1007480046d6cc285c2c01ae1ed42671e63
Craig Russell <craig@craig-russell.co.uk>
2015-07-31 21:49:42 +0100

    Added conparator method logic

--- app/models/rule.py
+++ app/models/rule.py
@@ -18,6 +18,27 @@ class Rule(db.Model):
     created_at = db.Column(db.DateTime, default=DT.now())
     updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
+
+    def compare(self, field_value):
+        """Compare configured value to field value using comparator"""
+        test_value = self.value # Easier to read variable name
+        try:
+            if   self.comparator == "MATCH":          return test_value == field_value
+            elif self.comparator == "NOT_MATCH":      return test_value != field_value
+            elif self.comparator == "CONTAIN":        return test_value in field_value
+            elif self.comparator == "NOT_CONTAIN":    return test_value not in field_value
+            elif self.comparator == "START_WITH":     return field_value.startswith(test_value)
+            elif self.comparator == "NOT_START_WITH": return not field_value.startswith(test_value)
+            elif self.comparator == "END_WITH":       return field_value.endswith(test_value)
+            elif self.comparator == "NOT_END_WITH":   return not field_value.endswith(test_value)
+            elif self.comparator == "LESS_THAN":      return int(test_value) < int(field_value)
+            elif self.comparator == "GREATER_THAN":   return int(test_value) > int(field_value)
+            else: return False
+        except Exception as e:
+            print e # TODO logging
+            return False
+
+
     def __repr__(self):
         return 'Rule[%r] %r, %r, %r, %r, %r, %r, %r' % \
             (self.id, self.segment_id, self.group_id, self.field, self.comparator, self.value, self.created_at.isoformat(), self.updated_at.isoformat())
