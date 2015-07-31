392d072b2df7a51a97147abf8a0bd5f231555bc2
Craig Russell <craig@craig-russell.co.uk>
2015-07-31 23:57:10 +0100

    Added methods to apply rules to visitor data

--- app/models/rule.py
+++ app/models/rule.py
@@ -18,6 +18,41 @@ class Rule(db.Model):
     created_at = db.Column(db.DateTime, default=DT.now())
     updated_at = db.Column(db.DateTime, default=DT.now(), onupdate=DT.now())
 
+    def apply(self, data):
+        """Returns true if this rule applies to any part of data, false otherwise"""
+        if self.apply_to_data(data): return True
+
+        for key, session in data.get('sessions', dict()).iteritems():
+            if self.apply_to_session(session): return True
+
+            for pageview in session.get('pageviews', list()):
+                if self.apply_to_pageview(pageview): return True
+
+        return False
+
+    def apply_to_data(self, data):
+        """Returns true if this rule applies to any data-level field, false otherwise"""
+        exceptions = ['visitor_id', 'account_id', 'sessions']
+        for key, value in data.iteritems():
+            if key not in exceptions and key == self.field and self.compare(value):
+                return True
+        return False
+
+    def apply_to_session(self, session):
+        """Returns true if this rule applies to any session-level field, false otherwise"""
+        exceptions = ['pageviews']
+        for key, value in session.iteritems():
+            if key not in exceptions and key == self.field and self.compare(value):
+                return True
+        return False
+
+    def apply_to_pageview(self, pageview):
+        """Returns true if this rule applies to any pageview-level field, false otherwise"""
+        exceptions = ['placeholders', 'prefix', 'script_version', 'event']
+        for key, value in pageview.iteritems():
+            if key not in exceptions and key == self.field and self.compare(value):
+                return True
+        return False
 
     def compare(self, field_value):
         """Compare configured value to field value using comparator"""
@@ -31,13 +66,12 @@ class Rule(db.Model):
             elif self.comparator == "NOT_START_WITH": return not field_value.startswith(test_value)
             elif self.comparator == "END_WITH":       return field_value.endswith(test_value)
             elif self.comparator == "NOT_END_WITH":   return not field_value.endswith(test_value)
-            elif self.comparator == "LESS_THAN":      return int(test_value) < int(field_value)
-            elif self.comparator == "GREATER_THAN":   return int(test_value) > int(field_value)
+            elif self.comparator == "LESS_THAN":      return float(test_value) < float(field_value)
+            elif self.comparator == "GREATER_THAN":   return float(test_value) > float(field_value)
             else: return False
         except Exception as e:
             print e # TODO logging
             return False
-
 
     def __repr__(self):
         return 'Rule[%r] %r, %r, %r, %r, %r, %r, %r' % \
--- test/test_models/test_rule.py
+++ test/test_models/test_rule.py
@@ -5,14 +5,51 @@
 
 import unittest
 import datetime as DT
+from time import time as epoch
 from app import db
 from app.models import Rule
 
 class TestModelRule(unittest.TestCase):
 
+    def setUp(self):
+        # Test data
+        pv = dict()
+        pv['page_url']         = "http://example.com"
+        pv['referrer_url']     = "http://example.com"
+        pv['page_title']       = "Foo Bar"
+        pv['timestamp']        = epoch()
+        pv['language']         = "en-GB"
+        pv['event']            = "pageload"
+        pv['placeholders']     = ['a', 'b', 'c`']
+        pv['prefix']           = "ape"
+        pv['script_version']   = "0.0.0"
+
+        sn = dict()
+        sn['session_start']    = epoch()
+        sn['session_end']      = epoch()
+        sn['session_duration'] = 100
+        sn['request_address']  = "http://example.com"
+        sn['user_agent']       = "FooBar"
+        sn['screen_color']     = 256
+        sn['screen_width']     = 1000
+        sn['screen_height']    = 1000
+        sn['pageviews']        = [pv]
+        sn['pageview_count']   = 1
+
+        data = dict()
+        data['visitor_id']     = ""
+        data['account_id']     = ""
+        data['sessions']       = {0:sn}
+        data['sessions_count'] = 1
+
+        self.pageview = pv
+        self.session  = sn
+        self.data     = data
+
+
     def test_rule_crud(self):
         # Create
-        rule = Rule(group_id=3, field="foo", comparator="EQUAL", value="foo bar")
+        rule = Rule(group_id=3, field="foo", comparator="MATCH", value="foo bar")
         db.session.add(rule)
         db.session.commit()
         self.assertIn(rule, Rule.query.all())
@@ -27,7 +64,7 @@ class TestModelRule(unittest.TestCase):
         rule = Rule.query.filter_by(id=rule.id).first()
         self.assertEqual(rule.group_id, 3)
         self.assertEqual(rule.field, "foo")
-        self.assertEqual(rule.comparator, "EQUAL")
+        self.assertEqual(rule.comparator, "MATCH")
         self.assertEqual(rule.value, "foo bar")
         
         # Update
@@ -35,13 +72,13 @@ class TestModelRule(unittest.TestCase):
         old_updated_at  = rule.updated_at
         rule.group_id   = 5
         rule.field      = "bar"
-        rule.comparator = "NOT_EQUAL"
+        rule.comparator = "NOT_MATCH"
         rule.value      = "bar foo"
         rule = Rule.query.filter_by(id=rule.id).first()
         self.assertIsInstance(rule, Rule)
         self.assertEqual(rule.group_id, 5)
         self.assertEqual(rule.field, "bar")
-        self.assertEqual(rule.comparator, "NOT_EQUAL")
+        self.assertEqual(rule.comparator, "NOT_MATCH")
         self.assertEqual(rule.value, "bar foo")
         self.assertEqual(rule.created_at, old_created_at)
         self.assertNotEqual(rule.updated_at, old_updated_at)
@@ -50,6 +87,91 @@ class TestModelRule(unittest.TestCase):
         db.session.delete(rule)
         count = Rule.query.filter_by(id=rule.id).count()
         self.assertEqual(0, count)
+
+
+    def test_apply(self):
+        rule_1 = Rule(field='sessions_count', comparator="MATCH", value=self.data['sessions_count'])
+        rule_2 = Rule(field='foo',            comparator="MATCH", value=self.data['sessions_count'])
+        rule_3 = Rule(field='sessions_count', comparator="MATCH", value="Foo")
+
+        self.assertTrue(rule_1.apply(self.data))
+        self.assertFalse(rule_2.apply(self.data))
+        self.assertFalse(rule_3.apply(self.data))
+
+        rule_1 = Rule(field='session_start', comparator="MATCH", value=self.session['session_start'])
+        rule_2 = Rule(field='foo',           comparator="MATCH", value=self.session['session_start'])
+        rule_3 = Rule(field='session_start', comparator="MATCH", value="Foo")
+
+        self.assertTrue(rule_1.apply(self.data))
+        self.assertFalse(rule_2.apply(self.data))
+        self.assertFalse(rule_3.apply(self.data))
+
+        rule_1 = Rule(field='page_url', comparator="MATCH", value=self.pageview['page_url'])
+        rule_2 = Rule(field='foo',      comparator="MATCH", value=self.pageview['page_url'])
+        rule_3 = Rule(field='page_url', comparator="MATCH", value="Foo")
+
+        self.assertTrue(rule_1.apply(self.data))
+        self.assertFalse(rule_2.apply(self.data))
+        self.assertFalse(rule_3.apply(self.data))
+
+
+    def test_apply_to_data(self):
+        rule_1 = Rule(field='visitor_id',     comparator="MATCH", value=self.data['visitor_id'])
+        rule_2 = Rule(field='account_id',     comparator="MATCH", value=self.data['account_id'])
+        rule_3 = Rule(field='sessions',       comparator="MATCH", value=self.data['sessions'])
+        rule_4 = Rule(field='sessions_count', comparator="MATCH", value=self.data['sessions_count'])
+
+        self.assertFalse(rule_1.apply_to_data(self.data))
+        self.assertFalse(rule_2.apply_to_data(self.data))
+        self.assertFalse(rule_3.apply_to_data(self.data))
+        self.assertTrue(rule_4.apply_to_data(self.data))
+
+
+    def test_apply_to_session(self):
+        rule_1 = Rule(field='session_start',    comparator="MATCH", value=self.session['session_start'])
+        rule_2 = Rule(field='session_end',      comparator="MATCH", value=self.session['session_end'])
+        rule_3 = Rule(field='session_duration', comparator="MATCH", value=self.session['session_duration'])
+        rule_4 = Rule(field='request_address',  comparator="MATCH", value=self.session['request_address'])
+        rule_5 = Rule(field='user_agent',       comparator="MATCH", value=self.session['user_agent'])
+        rule_6 = Rule(field='screen_color',     comparator="MATCH", value=self.session['screen_color'])
+        rule_7 = Rule(field='screen_width',     comparator="MATCH", value=self.session['screen_width'])
+        rule_8 = Rule(field='screen_height',    comparator="MATCH", value=self.session['screen_height'])
+        rule_9 = Rule(field='pageview_count',   comparator="MATCH", value=self.session['pageview_count'])
+        rule_0 = Rule(field='pageviews',        comparator="MATCH", value=self.session['pageviews'])
+
+        self.assertTrue(rule_1.apply_to_session(self.session))
+        self.assertTrue(rule_2.apply_to_session(self.session))
+        self.assertTrue(rule_3.apply_to_session(self.session))
+        self.assertTrue(rule_4.apply_to_session(self.session))
+        self.assertTrue(rule_5.apply_to_session(self.session))
+        self.assertTrue(rule_6.apply_to_session(self.session))
+        self.assertTrue(rule_7.apply_to_session(self.session))
+        self.assertTrue(rule_8.apply_to_session(self.session))
+        self.assertTrue(rule_9.apply_to_session(self.session))
+        self.assertFalse(rule_0.apply_to_session(self.session))
+
+
+    def test_apply_to_pageview(self):
+        rule_1 = Rule(field='page_url',       comparator="MATCH", value=self.pageview['page_url'])
+        rule_2 = Rule(field='referrer_url',   comparator="MATCH", value=self.pageview['referrer_url'])
+        rule_3 = Rule(field='page_title',     comparator="MATCH", value=self.pageview['page_title'])
+        rule_4 = Rule(field='timestamp',      comparator="MATCH", value=self.pageview['timestamp'])
+        rule_5 = Rule(field='language',       comparator="MATCH", value=self.pageview['language'])
+        rule_6 = Rule(field='event',          comparator="MATCH", value=self.pageview['event'])
+        rule_7 = Rule(field='placeholders',   comparator="MATCH", value=self.pageview['placeholders'])
+        rule_8 = Rule(field='prefix',         comparator="MATCH", value=self.pageview['prefix'])
+        rule_9 = Rule(field='script_version', comparator="MATCH", value=self.pageview['script_version'])
+
+        self.assertTrue(rule_1.apply_to_pageview(self.pageview))
+        self.assertTrue(rule_2.apply_to_pageview(self.pageview))
+        self.assertTrue(rule_3.apply_to_pageview(self.pageview))
+        self.assertTrue(rule_4.apply_to_pageview(self.pageview))
+        self.assertTrue(rule_5.apply_to_pageview(self.pageview))
+        self.assertFalse(rule_6.apply_to_pageview(self.pageview))
+        self.assertFalse(rule_7.apply_to_pageview(self.pageview))
+        self.assertFalse(rule_8.apply_to_pageview(self.pageview))
+        self.assertFalse(rule_9.apply_to_pageview(self.pageview))
+
 
     def test_compare(self):
         r_match          = Rule(comparator="MATCH",          value="FooBar")
@@ -82,4 +204,8 @@ class TestModelRule(unittest.TestCase):
         self.assertTrue(r_less_than.compare("20"))
         self.assertFalse(r_less_than.compare("5"))
         self.assertTrue(r_greater_than.compare("5"))
+        self.assertTrue(r_greater_than.compare("5.0"))
+        self.assertTrue(r_greater_than.compare(5.0))
         self.assertFalse(r_greater_than.compare("20"))
+        self.assertFalse(r_greater_than.compare("20.0"))
+        self.assertFalse(r_greater_than.compare(20.0))
