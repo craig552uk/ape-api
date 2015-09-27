73365005c2a060ea6f923a90e5348c71d7df2031
Craig Russell <craig@craig-russell.co.uk>
2015-09-27 22:34:04 +0100

    Built user api

--- app/routes/api/user.py
+++ app/routes/api/user.py
@@ -5,34 +5,76 @@
 
 from app import app, db
 from app.models import *
+from flask import request
 from flask_json import as_json
-
+from werkzeug.exceptions import NotFound, BadRequest
 
 @app.route('/api/1/users/', methods=['GET'])
 @as_json
 def user_list():
-    return {"message": "GET: List all users"}
+    users = User.query.all()
+    return {'data': [u.to_dict() for u in users]}
 
 
 @app.route('/api/1/users/', methods=['POST'])
 @as_json
 def user_add():
-    return {"message": "POST: Add an user"}
+    data        = request.get_json() or dict()
+    name        = data.get('name', str())
+    email       = data.get('email', str())
+    password    = data.get('password', str())
+
+    if not name:     raise BadRequest("Name required")
+    if not email:    raise BadRequest("Email required")
+    if not password: raise BadRequest("Password required")
+
+    user = User(name=name, email=email, password=password)
+
+    if 'account_ids' in data.keys():
+        users.accounts = Account.query.filter(Account.id.in_(data['account_ids'])).all()
+
+    db.session.add(user)
+    db.session.commit()
+    db.session.refresh(user)
+    return {"data": user.to_dict()}
 
 
 @app.route('/api/1/users/<user_id>/', methods=['GET'])
 @as_json
 def user_show(user_id):
-    return {"message": "GET: Show %s" % user_id}
+    user = User.query.filter_by(id=user_id).first()
+    if not user: raise NotFound("No user exists with id '%s'" % user_id)
+    return {"data": user.to_dict()}
 
 
 @app.route('/api/1/users/<user_id>/', methods=['POST'])
 @as_json
 def user_update(user_id):
-    return {"message": "POST: Update %s" % user_id}
+    user = User.query.filter_by(id=user_id).first()
+    if not user: raise NotFound("No user exists with id '%s'" % user_id)
+
+    data = request.get_json() or dict()
+    if 'name'     in data.keys(): user.name     = data['name']
+    if 'email'    in data.keys(): user.email    = data['email']
+    if 'password' in data.keys(): user.password = data['password']
+    if 'enabled'  in data.keys(): user.enabled  = data['enabled']
+    if 'admin'    in data.keys(): user.admin    = data['admin']
+    
+    if 'account_ids' in data.keys():
+        users.accounts = Account.query.filter(Account.id.in_(data['account_ids'])).all()
+
+    db.session.add(user)
+    db.session.commit()
+    db.session.refresh(user)
+    return {"data": user.to_dict()}
 
 
 @app.route('/api/1/users/<user_id>/', methods=['DELETE'])
 @as_json
 def user_delete(user_id):
-    return {"message": "DELETE: Delete %s" % user_id}
+    user = User.query.filter_by(id=user_id).first()
+    if not user: raise NotFound("No user exists with id '%s'" % user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+    return {"data": "User (%s) deleted" % user_id}
--- test/test_routes/test_api/test_users.py
+++ test/test_routes/test_api/test_users.py
@@ -4,31 +4,122 @@
 # Test API users
 
 import unittest
+import simplejson as json
 from app import app
+from app.models import User
 from . import assert_success_response, assert_error_response
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, BadRequest
 
 class TestAPIUserRoutes(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        self.headers = [('Content-Type', 'application/json')]
 
     def test_api_users_list(self):
         r = self.app.get('/api/1/users/')
-        self.assertEqual(200, r.status_code)
+        d = assert_success_response(self, r)
+
+        all_users = User.query.all()
+        self.assertIsInstance(d['data'], list)
+        self.assertEqual(len(all_users), len(d['data']))
 
     def test_api_users_add(self):
-        r = self.app.post('/api/1/users/')
-        self.assertEqual(200, r.status_code)
+        # Bad requests
+        r = self.app.post('/api/1/users/', headers=self.headers)
+        assert_error_response(self, r, BadRequest())
+
+        r = self.app.post('/api/1/users/', headers=self.headers, data='{}')
+        assert_error_response(self, r, BadRequest())
+
+        r = self.app.post('/api/1/users/', headers=self.headers, data='{"name":"Jim Jones"}')
+        assert_error_response(self, r, BadRequest())
+
+        r = self.app.post('/api/1/users/', headers=self.headers, data='{"email":"jim@jones.com"}')
+        assert_error_response(self, r, BadRequest())
+
+        r = self.app.post('/api/1/users/', headers=self.headers, data='{"password":"passw0rd"}')
+        assert_error_response(self, r, BadRequest())
+
+        # Good request
+        data = dict(name="Jim Jones", email="jim@example.com", password="passw0rd")
+        r = self.app.post('/api/1/users/', headers=self.headers, data=json.dumps(data))
+        b = assert_success_response(self, r).get('data')
+        self.assertEqual(b['type'], "user")
+        self.assertEqual(b['name'], data['name'])
+        self.assertEqual(b['email'], data['email'])
+        self.assertIsNotNone(b['password'])
+        self.assertTrue(b['enabled'])
+        self.assertFalse(b['admin'])
+        self.assertIsNotNone(b['id'])
+        self.assertIsNotNone(b['created_at'])
+        self.assertIsNotNone(b['updated_at'])
+        self.assertIsNone(b['last_login'])
+
+        c = User.authenticate(data['email'], data['password'])
+        self.assertEqual(c.name, data['name'])
+        self.assertEqual(c.email, data['email'])
 
     def test_api_users_show(self):
+        data = dict(name="Jim Jones", email="jim@example.com", password="passw0rd")
+        r = self.app.post('/api/1/users/', headers=self.headers, data=json.dumps(data))
+        u = assert_success_response(self, r).get('data')
+
+        # Bad request
         r = self.app.get('/api/1/users/foobar/')
-        self.assertEqual(200, r.status_code)
+        assert_error_response(self, r, NotFound())
+
+        # Good request        
+        r = self.app.get('/api/1/users/%s/' % u['id'])
+        d = assert_success_response(self, r)
+
+        self.assertEqual(d['data']['type'], "user")
+        self.assertEqual(d['data']['id'], u['id'])
+
 
     def test_api_users_update(self):
+        data = dict(name="Jim Jones", email="jim@example.com", password="passw0rd")
+        r = self.app.post('/api/1/users/', headers=self.headers, data=json.dumps(data))
+        u = assert_success_response(self, r).get('data')
+
+        # Bad requests
         r = self.app.post('/api/1/users/foobar/')
-        self.assertEqual(200, r.status_code)
+        assert_error_response(self, r, NotFound())
+
+        r = self.app.post('/api/1/users/%s/' % u['id'], headers=self.headers)
+        assert_error_response(self, r, BadRequest())
+
+        # Good request
+        data = dict(name="Jane", email="jane@foo.com", password="qw3rty", enabled=False, admin=True)
+        r = self.app.post('/api/1/users/%s/' % u['id'], headers=self.headers, data=json.dumps(data))
+
+        b = assert_success_response(self, r).get('data')
+        self.assertEqual(b['type'], "user")
+        self.assertEqual(b['name'], data['name'])
+        self.assertEqual(b['email'], data['email'])
+        self.assertEqual(b['enabled'], data['enabled'])
+        self.assertEqual(b['admin'], data['admin'])
+
+        c = User.query.filter_by(id=u['id']).first()
+        self.assertEqual(c.name, data['name'])
+        self.assertEqual(c.email, data['email'])
+        self.assertEqual(c.enabled, data['enabled'])
+        self.assertEqual(c.admin, data['admin'])
+
 
     def test_api_users_delete(self):
+        data = dict(name="Jim Jones", email="jim@example.com", password="passw0rd")
+        r = self.app.post('/api/1/users/', headers=self.headers, data=json.dumps(data))
+        u = assert_success_response(self, r).get('data')
+
+        # Bad request
         r = self.app.delete('/api/1/users/foobar/')
-        self.assertEqual(200, r.status_code)
+        assert_error_response(self, r, NotFound())
+
+        # Good request
+        r = self.app.delete('/api/1/users/%s/' % u['id'])
+        d = assert_success_response(self, r)
+
+        self.assertIn("deleted", d['data'])
+        self.assertIsNone(User.query.filter_by(id=u['id']).first())
+
